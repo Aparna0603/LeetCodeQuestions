b7967b8c2bb0d097a0e136b9ace37d719a508845
Aparna0603 <162182527+Aparna0603@users.noreply.github.com>
2024-08-22 19:18:21 +0530

    Add files via upload

---
+++ LeetCode/Bcomplement.java
@@ -0,0 +1,21 @@
+
+public class Bcomplement {
+    public static int findcomplement(int num) {
+        String str = Integer.toBinaryString(num);
+        String res = "";
+
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i)=='0') {
+                res += '1';
+            }
+            else {
+                res += '0';
+            }
+        }
+        return Integer.parseInt(res, 2);
+    }
+    public static void main(String args[]) {
+        int num = 5;
+        System.out.println(findcomplement(num));
+    }
+}
